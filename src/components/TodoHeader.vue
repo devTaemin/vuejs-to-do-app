0cbeb059ad011056f97ce256238a5d9b1a7f47bf
Taemin Im <devTaemin@gmail.com>
2021-10-21 17:37:10 +0900

    Update TodoHeader.vue

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>header</div>
+    <header>
+        <div>TODO it!</div>
+    </header>
 </template>
 
 
@@ -11,5 +13,9 @@ export default ({
 
 
 <style scoped>
-
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
